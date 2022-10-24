922b2fc5937c319b97bccb1794c024c97f9278b4
Arnab <arnabchatterjee912@gmail.com>
2022-10-24 14:49:30 +0530

    second

--- backend/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect('mongodb+srv://arnabreigns:arnabreigns@cluster0.qrtp9ci.mongodb.net/?retryWrites=true&w=majority',
-{useNewUrlParser: true}).then(()=>{console.log("Connected")},err=>{console.log(err)})
